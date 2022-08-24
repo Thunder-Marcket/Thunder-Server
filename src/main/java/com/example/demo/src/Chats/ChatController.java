0021d00ad280b014c8314a66627d89849421c33e
jinsoo <jinsoo335@naver.com>
2022-08-24 17:46:27 +0900

    Update Chat-Rooms List Up Func

--- src/main/java/com/example/demo/src/Chats/ChatController.java
+++ src/main/java/com/example/demo/src/Chats/ChatController.java
@@ -1,14 +1,21 @@
 package com.example.demo.src.Chats;
 
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.src.Chats.model.GetChatRoomListRes;
 import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/chats")
+@RequestMapping("/chat-rooms")
 public class ChatController {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -27,4 +34,26 @@ public class ChatController {
         this.chatService = chatService;
         this.jwtService = jwtService;
     }
+
+
+    /**
+     * 내가 대화한 번개톡방 리스트 가져오는 API
+     * [GET] /chats
+     * @return BaseResponse<List<GetChatRoomListRes>>
+     */
+    @ResponseBody
+    @GetMapping("")
+    public BaseResponse<List<GetChatRoomListRes>> getChatRoomList(){
+        try{
+            int userIdx = jwtService.getUserIdx();
+
+            List<GetChatRoomListRes> getChatRoomListRes = chatProvier.getChatRoomList(userIdx);
+            return new BaseResponse<>(getChatRoomListRes);
+        } catch (BaseException exception){
+            return new BaseResponse<>(exception.getStatus());
+        }
+    }
+
+
+
 }
--- src/main/java/com/example/demo/src/Chats/ChatDao.java
+++ src/main/java/com/example/demo/src/Chats/ChatDao.java
@@ -1,7 +1,58 @@
 package com.example.demo.src.Chats;
 
+import com.example.demo.src.Chats.model.GetChatRoomListRes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.List;
 
 @Repository
 public class ChatDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public List<GetChatRoomListRes> getChatRoomList(int userIdx) {
+        String getChatRoomListQuery = "select\n" +
+                "    (select U.profileImgUrl from Items I\n" +
+                "            inner join Users U on I.userIdx = U.userIdx\n" +
+                "            where I.itemIdx = CR.itemIdx) AS storeImageUrl,\n" +
+                "    (select U.userName from Items I\n" +
+                "                      inner join Users U on I.userIdx = U.userIdx\n" +
+                "                      where I.itemIdx = CR.itemIdx) AS storeName,\n" +
+                "    (select I.itemIdx from Items I where I.itemIdx = CR.itemIdx) AS itemIdx,\n" +
+                "    CR.chatRoomIdx,\n" +
+                "    (select I.userIdx from Items I where I.itemIdx = CR.itemIdx) AS storeUserIdx,\n" +
+                "    (select C.message from Chats C where C.chatRoomIdx = CR.chatRoomIdx order by C.createdAt desc limit 1) AS lastChat,\n" +
+                "    (select\n" +
+                "        case\n" +
+                "            when datediff(now(), C.createdAt) < 1 then concat(abs(hour(now()) - hour(C.createdAt)), '시간 전')\n" +
+                "            ELSE date_format(now(), '%c월 %d일')\n" +
+                "        END\n" +
+                "     from Chats C where C.chatRoomIdx = CR.chatRoomIdx) AS period\n" +
+                "\n" +
+                "\n" +
+                "from ChatRooms CR\n" +
+                "\n" +
+                "where CR.buyUserIdx = ? or ? in (select I.userIdx from Items I where I.itemIdx = CR.itemIdx);";
+        Object[] getChatRoomListParam = new Object[]{userIdx, userIdx};
+
+        return this.jdbcTemplate.query(getChatRoomListQuery,
+                (rs, rowNum) -> new GetChatRoomListRes(
+                        rs.getString("storeImageUrl"),
+                        rs.getString("storeName"),
+                        rs.getString("period"),
+                        rs.getString("lastChat"),
+                        rs.getInt("itemIdx"),
+                        rs.getInt("chatRoomIdx"),
+                        rs.getInt("storeUserIdx")
+                ), getChatRoomListParam);
+
+    }
 }
