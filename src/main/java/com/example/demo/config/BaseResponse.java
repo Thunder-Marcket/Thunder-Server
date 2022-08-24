2abb3aa2e18cb8b9710f0374515659f049e15f56
jinsoo <jinsoo335@naver.com>
2022-08-24 19:06:28 +0900

    result -> baseResult

--- src/main/java/com/example/demo/config/BaseResponse.java
+++ src/main/java/com/example/demo/config/BaseResponse.java
@@ -11,21 +11,21 @@ import static com.example.demo.config.BaseResponseStatus.SUCCESS;
 
 @Getter
 @AllArgsConstructor
-@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+@JsonPropertyOrder({"isSuccess", "code", "message", "baseResult"})
 public class BaseResponse<T> {
     @JsonProperty("isSuccess")
     private final Boolean isSuccess;
     private final String message;
     private final int code;
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private T result;
+    private T baseResult;
 
     // 요청에 성공한 경우
-    public BaseResponse(T result) {
+    public BaseResponse(T baseResult) {
         this.isSuccess = SUCCESS.isSuccess();
         this.message = SUCCESS.getMessage();
         this.code = SUCCESS.getCode();
-        this.result = result;
+        this.baseResult = baseResult;
     }
 
     // 요청에 실패한 경우
